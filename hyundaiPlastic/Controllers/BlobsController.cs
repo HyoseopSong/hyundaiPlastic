cfb839bc3fd889b914a428a1b192089c1c988aa3
HyoseopSong <csnvshs@naver.com>
2017-06-08 17:09:30 +0900

    기본정보 탑재 완료

---
+++ hyundaiPlastic/Controllers/BlobsController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.IO;
+
+namespace hyundaiPlastic.Controllers
+{
+    public class BlobsController : Controller
+    {
+        // GET: Blobs
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateBlobContainer()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("hyundaiplastic_AzureStorageConnectionString"));
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("hyundaiplastic");
+            ViewBag.Success = container.CreateIfNotExists();
+            ViewBag.BlobContainerName = container.Name;
+            return View();
+        }
+
+        public EmptyResult UploadBlob()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("hyundaiplastic_AzureStorageConnectionString"));
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("hyundaiplastic");
+            CloudBlockBlob blob = container.GetBlockBlobReference("blobexample");
+            
+            using (var fileStream = System.IO.File.OpenRead(@"C:\Project\IMG_8332.jpg"))
+            {
+                blob.UploadFromStream(fileStream);
+            }
+            return new EmptyResult();
+        }
+        public ActionResult ListBlobs()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("hyundaiplastic_AzureStorageConnectionString"));
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("hyundaiplastic");
+
+            List<string> blobs = new List<string>();
+            foreach(IListBlobItem item in container.ListBlobs(useFlatBlobListing:true))
+            {
+                if (item.GetType() == typeof(CloudBlockBlob))
+                {
+                    CloudBlockBlob blob = (CloudBlockBlob)item;
+                    
+                    blobs.Add(blob.Uri.ToString());
+                }
+                else if (item.GetType() == typeof(CloudPageBlob))
+                {
+                    CloudPageBlob blob = (CloudPageBlob)item;
+                    blobs.Add(blob.Name);
+                }
+                else if (item.GetType() == typeof(CloudBlobDirectory))
+                {
+                    CloudBlobDirectory dir = (CloudBlobDirectory)item;
+                    blobs.Add(dir.Uri.ToString());
+                }
+            }
+            return View(blobs);
+        }
+        public EmptyResult DownloadBlob()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("hyundaiplastic_AzureStorageConnectionString"));
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("hyundaiplastic");
+            CloudBlockBlob blob = container.GetBlockBlobReference("IMG_8316.jpg");
+            using (var fileStream = System.IO.File.OpenWrite(@"C:\Project\downloadedImage.jpg"))
+            {
+                blob.DownloadToStream(fileStream);
+            }
+            return new EmptyResult();
+        }
+    }
+}
--- hyundaiPlastic/Controllers/HomeController.cs
+++ hyundaiPlastic/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,14 +18,39 @@ namespace hyundaiPlastic.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "";
 
-            return View();
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("hyundaiplastic_AzureStorageConnectionString"));
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("product");
+
+            IDictionary<string, string> blobs = new Dictionary<string, string>();
+            foreach (IListBlobItem item in container.ListBlobs(useFlatBlobListing: true))
+            {
+                if (item.GetType() == typeof(CloudBlockBlob))
+                {
+                    CloudBlockBlob blob = (CloudBlockBlob)item;
+                    string[] temp = blob.Name.Split('.');
+                    blobs.Add(temp[1], blob.Uri.ToString());
+                }
+                else if (item.GetType() == typeof(CloudPageBlob))
+                {
+                    CloudPageBlob blob = (CloudPageBlob)item;
+                    blobs.Add(blob.Name, blob.Uri.ToString());
+                }
+                //else if (item.GetType() == typeof(CloudBlobDirectory))
+                //{
+                //    CloudBlobDirectory dir = (CloudBlobDirectory)item;
+                //    blobs.Add(dir.Uri.ToString());
+                //}
+            }
+
+            return View(blobs);
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "";
 
             return View();
         }
